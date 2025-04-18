25951e08a86f05c24a809d4b206fd73d58d76288
Raph Gallardo <draphick@gmail.com>
2025-04-18 15:53:04 -0700

    testing again

--- tests/dev_test.py
+++ tests/dev_test.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from bot.character_loader import (
     create_character,
     load_all_characters,
